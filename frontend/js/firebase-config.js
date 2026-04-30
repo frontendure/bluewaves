2d9cba5b69209eb1bafc4123f0d3c3dd80d5c3f2
Ajmal-Noushad <ajmalaxj@gmail.com>
2026-04-30 20:45:48 +0530

    connected firebase for login

---
+++ frontend/js/firebase-config.js
@@ -0,0 +1,11 @@
+// js/firebase-config.js
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAbCjHfZlP0u1W2Bfpkf_s0QklRGH7ITiM",
+  authDomain: "bluewaves-7db8f.firebaseapp.com",
+  projectId: "bluewaves-7db8f",
+  storageBucket: "bluewaves-7db8f.firebasestorage.app",
+  messagingSenderId: "996307039527",
+  appId: "1:996307039527:web:d8cfbe0a8c76c1914fa502",
+  measurementId: "G-PDESP0P4XL"
+};
